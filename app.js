36e805b6a0010f73769a87e94e7a004ce0ad59cf
Araya <49897854+techgirlonlinee@users.noreply.github.com>
2021-03-31 12:08:18 +0200

    cleaned up code

--- app.js
+++ app.js
@@ -184,8 +184,6 @@ function formatAMPM(date) {
 
 const line = document.querySelector(".day-indicator")
 line.innerHTML = formatAMPM(new Date)
-// line.style.transform = `translate(-50%, -50%)`
-
 }
 
 
